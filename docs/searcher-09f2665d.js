171ea5e73d6678fa8acd40880c7e2e49047f853b
DefaultName <default@email.com>
2026-09-20 01:55:03 +0900

    update

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-402a7461.js',
+                path_to_root + 'searchindex-f4911f8f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
